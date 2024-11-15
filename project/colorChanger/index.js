a4b5830f5fcfdc20641e39a466c3c3475e7d808b
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-15 21:25:11 +0530

    Project_ColorChanger

--- project/colorChanger/index.js
+++ project/colorChanger/index.js
@@ -1,11 +1,44 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
 
+buttons.forEach((button) => {
 
+  button.addEventListener("click", function (event) {
 
+    // console.log(event);
+    // console.log(event.target);
 
+    //// either we can use if condition
+    // if (event.target.id === "grey") {
+    //   body.style.backgroundColor = event.target.id;
+    // }
+    // if (event.target.id === "white") {
+    //   body.style.backgroundColor = event.target.id;
+    // }
+    // if (event.target.id === "blue") {
+    //   body.style.backgroundColor = event.target.id;
+    // }
+    // if (event.target.id === "yellow") {
+    //   body.style.backgroundColor = event.target.id;
+    // }
 
-console.log("hello baby how do you do")
-
-
-
-
+    ///// either we can use switch statement
+    switch (event.target.id) {
+      case "grey":
+        body.style.backgroundColor = event.target.id;
+        break;
+      case "white":
+        body.style.backgroundColor = event.target.id;
+        break;
+      case "blue":
+        body.style.backgroundColor = event.target.id;
+        break;
+      case "yellow":
+        body.style.backgroundColor = event.target.id;
+        break;
+      default:
+        break;
+    }
+  });
+});
 
